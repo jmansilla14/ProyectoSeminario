1867dca132bad4f8e693d8fd04de8532daac161d
jmansilla <jmansillas@miumg.edu.gt>
2024-10-31 15:22:24 -0600

    se inicio el proyecto así como se agregaron primeros funcionamientos

---
+++ front/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { privateGuard, publicGuard } from './shared/guards/auth.guard';
+
+export const routes: Routes = [
+    {
+        path: 'auth',
+        canActivate: [publicGuard()],
+        loadChildren: () => import('./auth/features/shell/auth.routes'),
+        //autentitacion
+    },
+    {
+        path: 'dashboard',
+        canActivate: [privateGuard()],
+        loadComponent: () => import('./dashboard/dashboard.component'),
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard',
+    }
+];
